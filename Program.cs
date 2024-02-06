37bd9d17f47325e20f9d8f7e0057e4901d467e58
21MMukrrumBinAkhtar <21MMukrrumBinAkhtar@B105-19>
2024-02-06 09:29:32 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which is greater 20 or 21");
+            string num = Console.ReadLine();
+                Console.WriteLine(num);
+            if (21 > 20) ;
+            {
+                Console.WriteLine("21 is greater than 20");
+            }
+        }
+    }
+}
